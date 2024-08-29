c424278eed1dd3c65bd88ad2f13acbea6f33dd0b
jesse <92591280+jessejes@users.noreply.github.com>
2024-08-29 09:50:41 +0200

    Initial Commit

---
+++ imprvbl-ios Watch App/imprvbl_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  imprvbl_iosApp.swift
+//  imprvbl-ios Watch App
+//
+//  Created by Jesse on 29.08.24.
+//
+
+import SwiftUI
+
+@main
+struct imprvbl_ios_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ imprvbl-ios/imprvbl_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  imprvbl_iosApp.swift
+//  imprvbl-ios
+//
+//  Created by Jesse on 29.08.24.
+//
+
+import SwiftUI
+
+@main
+struct imprvbl_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
